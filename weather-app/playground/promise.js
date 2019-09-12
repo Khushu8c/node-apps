c2e091cde6697d9fd4b0da7eafa18c422f9476c8
Soniya Patel <soniya.patel@infostretch.com>
2019-09-12 12:59:21 +0530

    added example of how promise chaining implemented

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -1,14 +1,42 @@
-var promise = new Promise((resolve, reject) => {
-   resolve('Hello is resolve');
-   reject('Hello is reject');
-   
-  setTimeout(() => {
-    resolve('Hello is resolve');
-  }, 2000)
-})
+var asyncAdd = (a,b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else {
+        reject('Arguments must be number');
+      }
+    }, 1500)
+  })
+};
 
-promise.then((message) => {
-  console.log('Success: ', message);
-}, (error) => {
-console.log('failur: ', error);
-})
+asyncAdd(1, '3').then((pass) => {
+  console.log('Result: ', pass)
+  return asyncAdd(pass, 6);
+}, (fail) => {
+  console.log(fail);
+}).then((pass) => {
+  console.log('final out put should be: ', pass)
+}, (error) =>{
+  console.log(error);
+});
+
+
+
+
+
+
+// var promise = new Promise((resolve, reject) => {
+//    resolve('Hello is resolve');
+//    reject('Hello is reject');
+//
+//   setTimeout(() => {
+//     resolve('Hello is resolve');
+//   }, 2000)
+// })
+//
+// promise.then((message) => {
+//   console.log('Success: ', message);
+// }, (error) => {
+// console.log('failur: ', error);
+// })
