26c03186ff933ff5251ce04cddf2471404222ac5
Soniya Patel <soniya.patel@infostretch.com>
2019-09-14 16:47:22 +0530

    added functionality of middleware in application so that we can log all request and respones

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
 
+hbs.registerHelper('toUpperCase', (text) => {
+  return text.toUpperCase();
+})
+
+app.use((req, res, next) => {
+  var time = `${new Date()} : ${req.method} ${req.url}`;
+  console.log(time);
+  fs.appendFile('server.log', time + '\n', (error) => {
+    console.log('Unable to append file')
+  });
+next();
+});
+
+app.use((req, res, next) => {
+res.render('maintainance.hbs');
+})
+
+app.use(express.static(__dirname + '/public'));
 app.get('/', (request, response) => {
   response.render('welcome.hbs', {
     pageTitle: "Welcome Page",
-    currentYear: new Date().getFullYear(),
     welcomeMsg: 'Hello Welcome to demo site'
   });
 });
@@ -18,8 +39,7 @@ app.get('/', (request, response) => {
 app.get('/about', (request, response) => {
   response.render('about.hbs', {
     pageTitle: 'About page!',
-    currentYear: new Date().getFullYear()
-
+    welcomeMsg: 'Hello Welcome to about page'
   });
 });
 
