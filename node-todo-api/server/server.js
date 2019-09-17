704aef1ad512752f3eadf35bd79ad33437eadb93
Soniya Patel <soniya.patel@infostretch.com>
2019-09-17 10:03:30 +0530

    added validation in model in mongoose schema for user database

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -3,24 +3,49 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('todo', {
-  text: {
-    type: String
+// var Todo = mongoose.model('todo', {
+//   text: {
+//     type: String
+//   },
+//   completed: {
+//     type: Boolean
+//   },
+//   completedAt: {
+//     type: Boolean
+//   }
+// });
+//
+// var todoOne = new Todo({
+//   text: 'Soniya'
+// });
+//
+// todoOne.save().then((doc) => {
+//   console.log('saved log',doc);
+// }, (err) => {
+//   console.log('Unable to add data: ', err);
+// });
+
+var User = mongoose.model('USER', {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    min: 1
   },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Boolean
+  age: {
+    type: Number,
+    required: true,
+    default: 10
   }
+})
+
+var user = new User({
+  name: '111      ssss  '
 });
 
-var todoOne = new Todo({
-  text: 'Soniya'
-});
-
-todoOne.save().then((doc) => {
-  console.log('saved log',doc);
-}, (err) => {
-  console.log('Unable to add data: ', err);
-});
+user.save().then((result) => {
+console.log('User data saved: ', result);
+}, (e) => {
+  console.log('Error ocuurs: ', e);
+})
