62daae12195bb77c7ce63983a8d493846585bb46
Soniya Patel <soniya.patel@infostretch.com>
2019-09-17 21:12:11 +0530

    added get method to retrieve all data from todo collection

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -24,6 +24,15 @@ app.post('/todos', (req, res) => {
   })
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+      res.send(todos);
+  }, (error) => {
+  res.status(400).send(error);
+})
+});
+
+
 app.listen(3000, () => {
   console.log('Server has started on port 3000.');
 });
